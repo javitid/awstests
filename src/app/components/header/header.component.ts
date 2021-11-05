7dc571c2ed534d91c59aafb72773ef6692d68958
Javier Garcia <javitid@gmail.com>
2021-11-05 16:51:33 +0100

    Add icons and toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  public toggle(): void {
+    // Menu toggle
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
 
-  constructor() { }
-
-  ngOnInit(): void {
+    navigation?.classList.toggle('active');
+    main?.classList.toggle('active');
   }
-
 }
