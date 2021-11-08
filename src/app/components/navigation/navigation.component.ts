96afb105acb77178685c73c8411b65d141bc3f43
Javier Garcia <javitid@gmail.com>
2021-11-09 00:34:06 +0100

    Remove unneded classes

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -6,21 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-  public hover = {
-    questions: false,
-    settings: false,
-  };
-
-  public hovered = {
-    questions: false,
-    settings: false,
-  };
-
-  public activeLink($event: any, indicator: string): void {
-    // Remove hover class in all the elements
-    let list = document.querySelectorAll('.navigation li');
-    list.forEach(item => {
-        item.classList.remove('hovered');
-    })
-  }
 }
