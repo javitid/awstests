abe70177a1d0fe7cc795f705e33af6ba02460ea9
Javier Garcia <jav.garcia.ext@orangebank.es>
2023-07-05 14:54:59 +0200

    Add new tests

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -9,6 +9,30 @@ export const ASSESSMENT_TYPE = {
 };
 
 export const QUESTIONARIES = [
+    'architectA1',
+    'architectA2',
+    'architectA3',
+    'architectA4',
+    'architectA5',
+    'architectA6',
+    'architectA-2023-1',
+    // 'architectA-2023-2',
+    // 'architectA-2023-3',
+    // 'architectA-2023-4',
+    // 'architectA-2023-5',
+    // 'architectA-2023-6',
+    'architectP1',
+    'architectP2',
+    'architectP3',
+    'architectP4',
+    'architectP5',
+    'architectP6',
+    'developerA1',
+    'developerA2',
+    'developerA3',
+    'developerA4',
+    'developerA5',
+    'developerA6',
     'practitioner1',   
     'practitioner2', 
     'practitioner3', 
@@ -20,13 +44,7 @@ export const QUESTIONARIES = [
     'practitioner2020-3',
     'practitioner2020-4',
     'practitioner2020-5',
-    'practitioner2020-6',
-    'architectA1',
-    'architectA2',
-    'architectA3',
-    'architectA4',
-    'architectA5',
-    'architectA6'
+    'practitioner2020-6'
 ];
 
 export const SECTION = [
